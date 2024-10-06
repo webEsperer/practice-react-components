fab75f649fe552cfb83dfae2bcb86b381c5ea920
Piotr Matejek <webesperer@gmail.com>
2024-10-06 05:41:24 +0200

    solved task 01

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
-    }
-    render() {
-        console.log('render');
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  // state = {
+  //     counter: 0,
+  // }
+  render() {
+    console.log("render");
 
-        return <h1>{ this.state.counter }</h1>
-    }
+    return <h1>{this.state.counter}</h1>;
+  }
+
+  componentDidMount() {
+    console.log("DidMount");
+    this.id = setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("DidUpdate");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.id);
+  }
 }
 
-root.render(<App/>);
+root.render(<App />);

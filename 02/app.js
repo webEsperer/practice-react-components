7cc8a12a0e0dbf5049c86ae310a6820678886b4b
Piotr Matejek <webesperer@gmail.com>
2024-10-06 05:48:25 +0200

    solved task 02

--- 02/app.js
+++ 02/app.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 
 class Counter extends React.Component {
-    state = {
-        amount: 0,
-    }
-    
-    render() {
-        return <button>click me ({ this.state.amount })</button>
-    }
+  state = {
+    amount: 0,
+  };
+
+  handleClick = () => {
+    this.setState({
+      amount: this.state.amount + 1,
+    });
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>click me ({this.state.amount})</button>
+    );
+  }
 }
 
 root.render(<Counter />);

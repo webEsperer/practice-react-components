31aff149225dd32f8ef690a65cacc2b7f6398e79
Piotr Matejek <webesperer@gmail.com>
2024-10-06 06:13:56 +0200

    solved task 03

--- 03/app.js
+++ 03/app.js
@@ -1,43 +1,66 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 
 class Article extends React.Component {
-    state = {
-        comments: [],
-    }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+  state = {
+    comments: ["text1", "text2"],
+    text: "",
+  };
+
+  showComments() {
+    return this.state.comments.map((comment, index) => {
+      return <li key={index}>{comment}</li>;
+    });
+  }
+
+  handleTextarea = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      comments: [...this.state.comments, this.state.text],
+      text: "",
+    });
+  };
+
+  render() {
+    const { title, body } = this.props;
+    return (
+      <article>
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <section>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>
+                <textarea
+                  style={{ minWidth: "300px", minHeight: "120px" }}
+                  name="content"
+                  value={this.state.text}
+                  onChange={this.handleTextarea}
+                />
+              </label>
+            </div>
+            <div>
+              <input type="submit" value="dodaj komentarz" />
+            </div>
+          </form>
+          <ul>{this.showComments()}</ul>
+        </section>
+      </article>
+    );
+  }
 }
 
 root.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />
+  <Article
+    title="Programowanie jest super!"
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
+  />
 );
